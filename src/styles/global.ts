5fda06cdaaa43ac609f1ce1efffc169f0f3075b0
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-09 22:09:11 -0300

    feat: creating global style and change icon

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle``;
+export const GlobalStyle = createGlobalStyle`
+ * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Inter', 'sans-serif';
+
+    &:focus {
+        outline: none;
+    }
+ }
+
+ body {
+    background-color: ${(props) => props.theme['system-dark-blue']};
+ }
+`;
