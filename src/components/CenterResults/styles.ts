7bdbce3eafebdb5bdaca1ac07ed8f10357c6376a
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-12 17:56:09 -0300

    fix: adjusting code

--- src/components/CenterResults/styles.ts
+++ src/components/CenterResults/styles.ts
@@ -21,7 +21,6 @@ export const CarouselContainer = styled.div`
 export const TextContainer = styled.div``;
 
 export const ItemsContainer = styled.div`
-  flex-wrap: wrap;
   gap: 16px;
   margin-top: 40px;
   display: grid;
