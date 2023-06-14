cda70ca2e8ac91114ccc2beb5efbe3299bb80580
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-13 23:41:01 -0300

    feat: add register form validation

--- src/components/Mailing/index.tsx
+++ src/components/Mailing/index.tsx
@@ -9,15 +9,36 @@ import {
 import { Text } from '../Text';
 import { Input } from '../Input';
 import { Check } from '@phosphor-icons/react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { ErrorMessage } from '../ErrorMessage';
 
 export function Mailing() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      language: '',
+      company: '',
+      email: '',
+      name: '',
+    },
+  });
   const colors = useTheme();
+
+  const onSubmit = () => {
+    toast.success('Recebemos o seu cadastro, seja bem vindo!');
+  };
+
   return (
     <Container>
       <Heading color={colors['primary-500']} tag="h1" weight="normal">
         Cadastre-se no Mailing
       </Heading>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Text
           tag="label"
           size="lg"
@@ -26,7 +47,12 @@ export function Mailing() {
         >
           Nome:
         </Text>
-        <Input placeholder="Ex: Maria Pereira" />
+        <Input
+          placeholder="Ex: Maria Pereira"
+          {...register('name', { required: 'Campo obrigatório' })}
+          onChange={(event) => setValue('name', event.target.value)}
+        />
+        {errors.name && <ErrorMessage>{errors.name?.message}</ErrorMessage>}
         <Text
           tag="label"
           size="lg"
@@ -35,7 +61,14 @@ export function Mailing() {
         >
           E-mail
         </Text>
-        <Input placeholder="Ex: maria@gmail.com" type="email" />
+        <Input
+          placeholder="Ex: maria@gmail.com"
+          type="email"
+          {...register('email', { required: 'Campo obrigatório' })}
+          onChange={(event) => setValue('email', event.target.value)}
+        />
+        {errors.email && <ErrorMessage>{errors.email?.message}</ErrorMessage>}
+
         <Text
           tag="label"
           size="lg"
@@ -44,7 +77,15 @@ export function Mailing() {
         >
           Empresa
         </Text>
-        <Input placeholder="" />
+        <Input
+          placeholder="Ex: TC Investimentos"
+          {...register('company', { required: 'Campo obrigatório' })}
+          onChange={(event) => setValue('company', event.target.value)}
+        />
+        {errors.company && (
+          <ErrorMessage>{errors.company?.message}</ErrorMessage>
+        )}
+
         <CheckboxContainer>
           <CheckboxStyled id="c1">
             <CheckboxIndicatorStyled>
