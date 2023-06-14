a74598783edea8fbe23a6279f7d6a683f11a7c23
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-13 23:29:13 -0300

    feat: adding toast lib

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import { defaultTheme } from './styles/theme/default';
 import { GlobalStyle } from './styles/global';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
+import { Toaster } from 'react-hot-toast';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            border: '1px solid #713200',
+            padding: '16px',
+            color: defaultTheme['primary-500'],
+          },
+        }}
+      />
       <BrowserRouter>
         <GlobalStyle />
         <Router />
