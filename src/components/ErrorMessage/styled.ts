a3902c0bdfd8b9fc9c3277ebf0f80a07faf45120
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-13 23:30:05 -0300

    feat: add form validation

---
+++ src/components/ErrorMessage/styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const ErrorMessageText = styled.span`
+  color: ${(props) => props.theme['system-red']};
+  font-size: 0.75rem;
+`;
--- src/components/TalkWithRI/index.tsx
+++ src/components/TalkWithRI/index.tsx
@@ -6,13 +6,15 @@ import { Text } from '../Text';
 import { Input } from '../Input';
 import { useForm } from 'react-hook-form';
 import { OptionsSelectData } from '../../data/OptionsSelectData';
+import { toast } from 'react-hot-toast';
+import { ErrorMessage } from '../ErrorMessage';
 
 export function TalkWithRI() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
-    clearErrors,
   } = useForm({
     defaultValues: {
       subject: '',
@@ -23,7 +25,7 @@ export function TalkWithRI() {
   const colors = useTheme();
 
   const onSubmit = () => {
-    console.log('aqui');
+    toast.success('Recebemos o seu contato, em breve lhe retornaremos!');
   };
 
   return (
@@ -33,11 +35,15 @@ export function TalkWithRI() {
       </Heading>
       <form onSubmit={handleSubmit(onSubmit)}>
         <SelectTalkToRI
-          {...register('subject', { required: 'Campo Obrigatorio' })}
+          {...register('subject', { required: 'Campo obrigatório' })}
           options={OptionsSelectData}
           placeholder="Escolha o Assunto"
           title="Assunto"
+          onChange={(event) => setValue('subject', event.target.value)}
         />
+        {errors.subject && (
+          <ErrorMessage>{errors.subject?.message}</ErrorMessage>
+        )}
         <Text
           tag="label"
           size="lg"
@@ -47,9 +53,11 @@ export function TalkWithRI() {
           Nome:
         </Text>
         <Input
-          {...register('name', { required: 'Campo Obrigatorio' })}
+          {...register('name', { required: 'Campo obrigatório' })}
           placeholder="Ex: Maria Pereira"
+          onChange={(event) => setValue('name', event.target.value)}
         />
+        {errors.name && <ErrorMessage>{errors.name?.message}</ErrorMessage>}
         <Text
           tag="label"
           size="lg"
@@ -59,10 +67,12 @@ export function TalkWithRI() {
           E-mail
         </Text>
         <Input
-          {...register('email', { required: 'Campo Obrigatorio' })}
+          {...register('email', { required: 'Campo obrigatório' })}
           placeholder="Ex: maria@gmail.com"
           type="email"
+          onChange={(event) => setValue('email', event.target.value)}
         />
+        {errors.email && <ErrorMessage>{errors.email?.message}</ErrorMessage>}
 
         <button type="submit">Enviar</button>
       </form>
